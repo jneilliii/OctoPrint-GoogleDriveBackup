7b7de59f4965c5121ab17c25b0898334ebc9a982
jneilliii <jneilliii+github@gmail.com>
2020-09-17 10:18:27 -0400

    0.0.1
    Initial Release

---
+++ octoprint_googledrivebackup/__init__.py
@@ -0,0 +1,129 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class GoogledrivebackupPlugin(octoprint.plugin.SettingsPlugin,
+                              octoprint.plugin.AssetPlugin,
+                              octoprint.plugin.TemplatePlugin,
+							  octoprint.plugin.EventHandlerPlugin,
+							  octoprint.plugin.SimpleApiPlugin):
+
+	##~~ SettingsPlugin mixin
+
+	def __init__(self):
+		self.gauth = None
+
+	def get_settings_defaults(self):
+		return dict(
+			cert_saved=False,
+			cert_authorized=False
+		)
+
+	##~~ SimpleApiPlugin mixin
+
+	def get_api_commands(self):
+		return dict(gen_secret=["json_data"], authorize=["auth_code"])
+
+	def on_api_command(self, command, data):
+		from octoprint.server import user_permission
+		import flask
+		if not user_permission.can():
+			return flask.make_response("Insufficient rights", 403)
+
+		from pydrive.auth import GoogleAuth
+		config_file = "{}/client_secrets.json".format(self.get_plugin_data_folder())
+		credentials_file = "{}/credentials.json".format(self.get_plugin_data_folder())
+		if not self.gauth:
+			self.gauth = GoogleAuth()
+
+		if command == "gen_secret":
+			import json
+			# write out our client_secrets.json file
+			with open(config_file, "w") as f:
+				f.write(json.dumps(data["json_data"]))
+			self._settings.set(["cert_saved"], True)
+			self._settings.save()
+
+			self.gauth.LoadClientConfigFile(config_file)
+			auth_url = self.gauth.GetAuthUrl()
+			return flask.jsonify(dict(cert_saved=True, url=auth_url))
+
+		if command == "authorize":
+			self._logger.info("Attempting to authorize Google App")
+			if not self.gauth:
+				return flask.jsonify(dict(authorized=False))
+			# Try to load saved client credentials
+			self.gauth.Auth(data["auth_code"])
+			self.gauth.SaveCredentialsFile(credentials_file)
+			self._settings.set(["cert_authorized"], True)
+			self._settings.save()
+			return flask.jsonify(dict(authorized=True))
+
+		##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return dict(
+			js=["js/googledrivebackup.js"]
+		)
+
+	##~~ EventHandlerPlugin mixin
+
+	def on_event(self, event, payload):
+		if event == "plugin_backup_backup_created" and self._settings.get_boolean(["cert_authorized"]):
+			self._logger.info("{} created, will now attempt to upload to Google Drive".format(payload["path"]))
+			from pydrive.drive import GoogleDrive
+			from pydrive.auth import GoogleAuth
+			credentials_file = "{}/credentials.json".format(self.get_plugin_data_folder())
+			gauth = GoogleAuth()
+			gauth.LoadCredentialsFile(credentials_file)
+			if gauth.credentials is None:
+				self._logger.error("not authorized")
+				self._settings.set(["cert_authorized"], False)
+				self._settings.save()
+				return
+			elif gauth.access_token_expired:
+				gauth.Refresh()
+			else:
+				gauth.Authorize()
+			gauth.SaveCredentialsFile(credentials_file)
+			drive = GoogleDrive(gauth)
+			f = drive.CreateFile({'title': payload["name"]})
+			f.SetContentFile(payload["path"])
+			f.Upload()
+			f = None
+
+		##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		return dict(
+			googledrivebackup=dict(
+				displayName="Google Drive Backup",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-GoogleDriveBackup",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-GoogleDriveBackup/archive/{target_version}.zip"
+			)
+		)
+
+
+__plugin_name__ = "Google Drive Backup"
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = GoogledrivebackupPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+
---
+++ octoprint_googledrivebackup/static/js/googledrivebackup.js
@@ -0,0 +1,121 @@
+/*
+ * View model for Google Drive Backup
+ *
+ * Author: jneilliii
+ * License: MIT
+ */
+$(function() {
+    function GoogledrivebackupViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        self.settingsViewModel = parameters[0];
+        self.cert_saved = ko.observable(false);
+        self.cert_authorized = ko.observable(false);
+        self.authorizing = ko.observable(false);
+        self.cert_file_name = ko.observable('');
+        self.cert_file_data = undefined;
+        self.auth_code = ko.observable('');
+        self.auth_url = ko.observable('#');
+
+        var certFileuploadOptions = {
+            dataType: "json",
+            maxNumberOfFiles: 1,
+            autoUpload: false,
+            headers: OctoPrint.getRequestHeaders(),
+            add: function(e, data) {
+                if (data.files.length === 0) {
+                    // no files? ignore
+                    return false;
+                }
+
+                self.cert_file_name(data.files[0].name);
+                self.cert_file_data = data;
+            },
+            done: function(e, data) {
+                self.cert_file_name(undefined);
+                self.cert_file_data = undefined;
+            }
+        };
+
+        $("#googledrivebackup_cert_file").fileupload(certFileuploadOptions);
+
+        self.onBeforeBinding = function() {
+			self.cert_saved(self.settingsViewModel.settings.plugins.googledrivebackup.cert_saved());
+			self.cert_authorized(self.settingsViewModel.settings.plugins.googledrivebackup.cert_authorized());
+		}
+
+        self.uploadCertFile = function(){
+            if (self.cert_file_data === undefined) return;
+            self.authorizing(true);
+            var input, file, fr;
+
+            if (typeof window.FileReader !== 'function') {
+              alert("The file API isn't supported on this browser yet.");
+              self.authorizing(false);
+              return;
+            }
+
+            file = self.cert_file_data.files[0];
+            fr = new FileReader();
+            fr.onload = receivedText;
+            fr.readAsText(file);
+
+            function receivedText(e) {
+                let lines = e.target.result;
+                var json_data = JSON.parse(lines);
+                $.ajax({
+                    url: API_BASEURL + "plugin/googledrivebackup",
+                    type: "POST",
+                    dataType: "json",
+                    data: JSON.stringify({command: "gen_secret", json_data: json_data}),
+                    contentType: "application/json; charset=UTF-8"
+                }).done(function(data){
+                    if(data.cert_saved){
+                        self.cert_saved(true);
+                        self.auth_url(data.url);
+                        self.authorizing(false);
+                    }
+                }).fail(function(data){
+                    console.log("error uploading cert file");
+                    self.authorizing(false);
+                });
+            }
+        }
+
+        self.authorizeCertFile = function(){
+            if(self.auth_code() === '') return;
+            self.authorizing(true);
+            $.ajax({
+                url: API_BASEURL + "plugin/googledrivebackup",
+                type: "POST",
+                dataType: "json",
+                data: JSON.stringify({command: "authorize", auth_code: self.auth_code()}),
+                contentType: "application/json; charset=UTF-8"
+            }).done(function(data){
+                if(data.authorized){
+                    self.cert_authorized(true);
+                    self.authorizing(false);
+                }
+            }).fail(function(data){
+                console.log("error authorizing");
+                self.cert_authorized(false);
+                self.authorizing(false);
+            });
+        }
+
+        self.deleteCertFiles = function(){
+            self.cert_saved(false);
+			self.cert_authorized(false);
+        }
+    }
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: GoogledrivebackupViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ "settingsViewModel" ],
+        // Elements to bind to, e.g. #settings_plugin_googledrivebackup, #tab_plugin_googledrivebackup, ...
+        elements: [ "#settings_plugin_googledrivebackup" ]
+    });
+});
