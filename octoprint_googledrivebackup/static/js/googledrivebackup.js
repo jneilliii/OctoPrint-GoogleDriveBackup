6d04495dd207da8fe28a84963e335a7175aa8440
Andrew A <andrewadev@gmail.com>
2022-12-31 16:02:05 +0100

    Extract & update step number
    Changes to the readme changed the relevant step number, so reflect that here.
    
    Also extract the number to its own variable, so that it is easy to update.

--- octoprint_googledrivebackup/static/js/googledrivebackup.js
+++ octoprint_googledrivebackup/static/js/googledrivebackup.js
@@ -6,12 +6,13 @@
  */
 const redirectHost = `https://jneilliii.github.io`;
 const redirectUri = `${redirectHost}/OctoPrint-GoogleDriveBackup/`;
+const redirectStep = '15'
 
 const isCorrectRedirectUri = (setting) => setting === redirectUri;
 
 const getRedirectUriFeedback = (setting) => {
     const linkText =
-        '<a target="_blank" href="https://github.com/jneilliii/OctoPrint-GoogleDriveBackup#create-a-google-oauth-app">step 13</a>.';
+        `<a target="_blank" href="https://github.com/jneilliii/OctoPrint-GoogleDriveBackup#create-a-google-oauth-app">step ${15}</a>.`;
     if (setting) {
         if (!setting.startsWith(redirectHost)) {
             return `Please use the host '${redirectHost}' for Authorized Redirect URI as specified in ${linkText}`;
@@ -19,7 +20,7 @@ const getRedirectUriFeedback = (setting) => {
             return `Make sure the Authorized Redirect URI ends with a trailing slash as specified in ${linkText}`;
         }
     }
-    return `Missing Authorized Redirect URI "${redirectUri}" in <a target="_blank" href="https://github.com/jneilliii/OctoPrint-GoogleDriveBackup#create-a-google-oauth-app">step 13</a>.`;
+    return `Missing Authorized Redirect URI "${redirectUri}" in ${linkText}`;
 };
 
 $(function() {
